0c8ff5bfa25de5ea0474a0c0c044982f3efd45c2
Anastasiia <124083730+anastasiiaschewtschenko@users.noreply.github.com>
2023-02-02 13:20:05 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MainClass
+{
+    class Program
+    {
+        static void Main()
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("{0}, твой год рождения {1}", name, 2023 - age);
+
+        }
+    }
+}
